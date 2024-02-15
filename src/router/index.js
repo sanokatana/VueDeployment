fdd90a8b276e2b085d5c6fc0568bc50ce6be13dd
sanokatana <chandrazahran@gmail.com>
2024-02-15 11:26:55 +0700

    Try Web History

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import store from '../store'
 
@@ -124,7 +124,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(process.env.BASE_URL),
+  history: createWebHistory(process.env.BASE_URL),
   routes
 })
 
